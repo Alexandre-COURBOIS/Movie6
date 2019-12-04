839d3ae4410de7eab3360c248b0302e4b4ba6be1
Thom076 <47177106+Thom076@users.noreply.github.com>
2019-12-04 16:45:01 +0100

    Index images des films

--- index.php
+++ index.php
@@ -12,10 +12,13 @@ $movies = $query->fetchAll();
 include('inc/header.php'); ?>
 
     <h1>Home</h1>
+
 <?php foreach ($movies as $movie) { ?>
     <div class="movies">
         <h2><?php echo $movie['title']; ?></h2>
+        <img src="posters/<?php echo $movie['id']; ?>.jpg" alt="">
     </div>
+
 <?php } ?>
 
 <?php include('inc/footer.php'); ?>
