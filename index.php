f52a0177e9efcc4b2ddbd11c3535ffadfe30180d
Thom076 <47177106+Thom076@users.noreply.github.com>
2019-12-04 14:22:15 +0100

    Correction index

--- index.php
+++ index.php
@@ -3,32 +3,22 @@ session_start();
 
 require('inc/pdo.php');
 require('inc/functions.php');
-$title = 'Home page';
+/*$title = 'Home page';
 $query = $pdo->prepare($sql);
 $query->execute();
-$reves = $query->fetchAll();
+$reves = $query->fetchAll();*/
 
 include('inc/header.php'); ?>
 
 <!DOCTYPE html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wonder</title>
-    <meta name="description" content="">
-
-    <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
 
 <section class="movies">
-    <div class="moviesview">
-        <ul class="slides">
+    <div class="movies2">
+        <ul class="movies3">
             <li><img src="???" alt=""></li>
         </ul>
     </div>
     <p class="lead">Ceci est un site pour les films</p>
-
 
     <div class="clear"></div>
 </section>
