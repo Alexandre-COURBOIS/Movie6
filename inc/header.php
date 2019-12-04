aff357e29a647a18048852bd3a853809356a8a27
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-04 16:05:02 +0100

    Initiation header php et css

--- inc/header.php
+++ inc/header.php
@@ -2,25 +2,34 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <titleMovies
-    </title>
-    <link ref="stylesheet" href="assets/css/style.css"
+    <titleMovies</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
 </head>
+
 <body>
 
-<div class="wrap">
+<div class="bg-header">
+    <div class="wrap">
 
-
-    <div class="Bg-header">
         <header>
-
-
-
-
-
+            <a href="" class="logo"><img src="assets/img/logowr.png" alt=""></a>
+            <nav class="nav navbar">
+                <ul>
+                    <li><a href="">accueil</a></li>
+                    <li><a href="">what's on</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">contact</a></li>
+                </ul>
+            </nav>
+            <div class="log">
+                <a class="connexion" href="">Connexion</a>
+                <a class="inscription" href="inscription.php">Inscription</a>
+                <a href=""><img src="assets/img/loupe.svg" alt=""></a>
+            </div>
         </header>
+        <div class="clear"></div>
     </div>
-
 </div>
 
 </body>
