c6448dd3d9782c8b8f05f486e35817a128325559
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-04 18:22:51 +0100

    inscription, connexion, deconnexion ok

--- inc/header.php
+++ inc/header.php
@@ -13,22 +13,37 @@
 
 <div class="bg-header">
     <div class="wrap">
-
+    <?php if(empty($_SESSION)) { ?>
         <header>
             <a href="" class="logo"><img src="assets/img/logowr.png" alt=""></a>
             <nav class="nav navbar">
                 <ul>
-                    <li><a href="">accueil</a></li>
+                    <li><a href="index.php">accueil</a></li>
                     <li><a href="">what's on</a></li>
                     <li><a href="">news</a></li>
                     <li><a href="">contact</a></li>
                 </ul>
             </nav>
             <div class="log">
-                <a class="connexion" href="">Connexion</a>
+                <a class="connexion" href="connexion.php">Connexion</a>
                 <a class="inscription" href="inscription.php">Inscription</a>
                 <a href=""><img src="assets/img/loupe.svg" alt=""></a>
             </div>
+            <?php } else { ?>
+            <a href="" class="logo"><img src="assets/img/logowr.png" alt=""></a>
+            <nav class="nav navbar">
+                <ul>
+                    <li><a href="index.php">accueil</a></li>
+                    <li><a href="">what's on</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">contact</a></li>
+                </ul>
+            </nav>
+            <div class="log">
+                <a class="connexion" href="deconnexion.php">Deconnexion</a>
+                <a href=""><img src="assets/img/loupe.svg" alt=""></a>
+            </div>
+            <?php } ?>
         </header>
         <div class="clear"></div>
     </div>
