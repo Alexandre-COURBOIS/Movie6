542feb7a7e38be348ca9da3e0d3cd644425c2b18
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-04 12:30:18 +0100

    Initiation header php et css

--- inc/header.php
+++ inc/header.php
@@ -2,10 +2,32 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <titleMovies</title>
+    <titleMovies
+    </title>
     <link ref="stylesheet" href="assets/css/style.css"
 </head>
 <body>
 
+<div class="wrap">
+
+
+    <div class="Bg-header">
+        <header>
+
+
+
+
+
+        </header>
+    </div>
+
+</div>
+
 </body>
+
+
+
+
+
+
 </html>
