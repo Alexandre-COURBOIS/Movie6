b438017283bd56633c3bb849bd3ce918d32adf88
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-05 11:09:26 +0100

    ajout de tout les détails ok

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include('inc/header.php'); ?>
 
     echo '<ul class="film">';
 
-    echo '<li><a href="details.php?id='.$movie['id'].'"><img src="posters/'.$movie['id'].'.jpg" alt=""</a></li>';
+    echo '<li><a href="details.php?id='.$movie['id'].'"><img src="posters/'.$movie['id'].'.jpg" alt=""></a></li>';
 
     echo '<ul>';
 
