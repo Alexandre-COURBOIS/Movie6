b31b2d845b1783a3973334281e0ab8fd54963d6e
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-11 09:53:01 +0100

    formulaire pour la note inséré en HTML, reste à faire l'insertion en BDD

--- favoris.php
+++ favoris.php
@@ -25,6 +25,14 @@ if (is_logged()) {
     die('403');
 }
 
+$select = array(
+    'Note'  => "1",
+    'Note1' => "2",
+    'Note2' => "3",
+    'Note3' => "4",
+    'Note4' => "5",
+);
+
 
 include('inc/header.php');
 
@@ -37,9 +45,23 @@ foreach ($movies as $movie) {
 
     echo '<ul>';
 
-    echo '<div><a href="delate-fav.php?id=' . $iduser . '&film=' . $movie['movieid'] . '">Retirer de mes favoris</a></div>';
+    echo '<div><a href="delate-fav.php?id=' . $iduser . '&film=' . $movie['movieid'] . '">Retirer de mes favoris</a></div>';?>
 
-}
+    <div class="formulaire select">
+        <label class="label-contact" for="select">Note /5 :</label>
+        <select id="select" name="select">
+            <option value="">-- Sélectionnez --</option>
+            <?php foreach ($select as $key => $value) { ?>
+            <option value="<?php echo $key; ?>"<?php if (!empty($_POST['select'])) {if($_POST['select'] == $key ) {echo ' selected="selected" ';}} ?>>
+                <?php echo $value; ?></option><?php   }  ?>
+        </select>
+        <input type="submit" name="submitted" value="Envoyer">
+        <span class="error"><?php if (!empty($errors['select'])) {echo $errors['select'];} ?></span>
+    </div>
+
+
+
+<?php }
 
     echo '<div class="clear"></div>';
 
